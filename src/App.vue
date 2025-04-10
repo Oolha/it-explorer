3a465ee495bba98a542e9476b20c1357993287e9
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-04-10 12:12:23 +0200

    add base Favorite place style / edit and delete btns

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
-import IButton from './components/IButton/IButton.vue'
+import HomepageView from './views/HomapageView.vue'
+import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
 </script>
 
 <template>
-  <main class="flex h-screen">
-    <section class="flex-1 flex justify-center items-center px-5 bg-[#F3743D]">
-      <div class="text-white text-center">
-        <img class="inline mb-6" src="./assets/img/map-pin.svg" alt="map pin" />
-        <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
-        <p class="leading-6 mb-11">
-          Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
-          ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
-        </p>
-        <IButton></IButton>
-      </div>
-    </section>
-    <section class="flex-1">
-      <img class="h-full object-cover" src="./assets/img/static-map.png" alt="static map" />
-    </section>
-  </main>
+  <!-- <HomepageView /> -->
+  <div class="bg-white h-screen w-[400px]">
+    <FavoritePlace />
+  </div>
 </template>
---
+++ src/components/FavoritePlace/FavoritePlaceIconButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="text-color-[#2C2C2C] cursor-pointer hover:text-[#F3743D]">
+    <slot> </slot>
+  </button>
+</template>
