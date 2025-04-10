2aa5f0cb6eadb69814522eb7339ce8aa15bbe34d
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-04-10 12:31:06 +0200

    add Favorite Places list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import HomepageView from './views/HomapageView.vue'
-import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
   <!-- <HomepageView /> -->
   <div class="bg-white h-screen w-[400px]">
-    <FavoritePlace />
+    <FavoritePlaces />
   </div>
 </template>
---
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
+import IButton from '../IButton/IButton.vue'
+</script>
+
+<template>
+  <div class="px-6 mb-4">
+    <div class="text-color-[#2C2C2C]">Додані маркери</div>
+    <FavoritePlace v-for="n in 4" :key="n" />
+    <IButton class="w-full mt-10">Додати маркер</IButton>
+  </div>
+</template>
